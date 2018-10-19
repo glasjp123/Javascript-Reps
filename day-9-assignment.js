6e1f656f21aceb6d6fb306a7319c77e5d814a981
glasjp123 <jwilki02@gmail.com>
2018-10-18 23:19:23 -0400

    Add comments

--- day-9-assignment.js
+++ day-9-assignment.js
@@ -1,23 +1,26 @@
 var numA = 62,
 	numB = 28;
 
-console.log(numA + numB);
+console.log(numA + numB);		//this solves problem 1a
 
 var string1 = "hello,",
 	string2 = " world";
 
-console.log(string1 + string2);
+console.log(string1 + string2);		//this solves problem 1b
 
 var array1 = ["Phillies", "Eagles", "Sixers", "Flyers"];
 
 console.log(array1[2]);
-console.log(array1[3]);
+console.log(array1[3]);		//this solves problem 2
 
 var newNumber = 97;
 
 if (newNumber < 100) {
 	alert("This number is less than 100")
 } else if (newNumber > 100) {
-	alert("This number is greater than 100")
-};
+	alert("This number is greater than 100")		
+};							//this solves problem 3
+
+
+
 
