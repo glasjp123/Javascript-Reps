bc2c4e5f641f903da507a432041673549e05f295
glasjp123 <jwilki02@gmail.com>
2018-10-18 20:48:50 -0400

    Begin Assignment but not finished yet

---
+++ day-9-assignment.js
@@ -0,0 +1,23 @@
+var numA = 62,
+	numB = 28;
+
+console.log(numA + numB);
+
+var string1 = "hello,",
+	string2 = " world";
+
+console.log(string1 + string2);
+
+var array1 = ["Phillies", "Eagles", "Sixers", "Flyers"];
+
+console.log(array1[2]);
+console.log(array1[3]);
+
+var newNumber = 97;
+
+if (newNumber < 100) {
+	alert("This number is less than 100")
+} else if (newNumber > 100) {
+	alert("This number is greater than 100")
+};
+
